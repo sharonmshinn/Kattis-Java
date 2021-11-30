2350cfc3c4769506a1cc0011f4bdae00c1f33fc8
Sharon Shin <89420953+sharonmshinn@users.noreply.github.com>
2021-11-30 13:37:33 -0600

    Update MirrorImages.java

--- MirrorImages.java
+++ MirrorImages.java
@@ -1,16 +1,22 @@
+//https://open.kattis.com/problems/mirror
+
 import java.util.Scanner;
 public class MirrorImages {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		//take input for num of cases
 		int cases = scanner.nextInt();
 		
+		//for number of cases
 		for(int i=1; i<=cases; i++) {
 			System.out.println("Test " + i);
+			//take input for num of rows and columns
 			int rows = scanner.nextInt();
 			int columns = scanner.nextInt();
 			String[] input = new String[rows];
 			String[] splitInput = new String[columns];
+			//reverse for loop to mirror image
 			for(int j=rows-1; j>=0; j--) {
 				input[j] = scanner.next();
 			}
@@ -23,5 +29,6 @@ public class MirrorImages {
 			}
 
 		}
+		scanner.close()
 	}
 }
